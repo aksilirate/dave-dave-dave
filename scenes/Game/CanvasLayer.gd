9866986f9be49b0e3e12716390975301bb55b647
axilirate <56825186+axilirate@users.noreply.github.com>
2021-11-26 11:59:20 +0200

    update

--- scenes/Game/CanvasLayer.gd
+++ scenes/Game/CanvasLayer.gd
@@ -1,5 +1,17 @@
 extends CanvasLayer
 
 
+onready var pause_menu = $PauseMenu
+
+
+func _ready():
+	pause_menu.hide()
+
 func _input(event):
-	pass
+	if event is InputEventKey:
+		if Input.is_action_pressed("esc"):
+			pause_menu.visible = !pause_menu.visible
+
+
+func _on_ResumeButton_pressed():
+	pause_menu.hide()
