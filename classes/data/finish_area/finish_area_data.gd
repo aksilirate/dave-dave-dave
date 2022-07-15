a02513a53ccb3c5002d52577449a02888c7ccf94
axilirate <56825186+axilirate@users.noreply.github.com>
2022-07-16 00:08:52 +0300

    update

---
+++ classes/data/finish_area/finish_area_data.gd
@@ -0,0 +1,9 @@
+class_name FinishAreaData
+extends DataResource
+
+
+
+signal activated
+
+
+
---
+++ scenes/objects/FinishArea2D/FinishArea2D.gd
@@ -0,0 +1,9 @@
+class_name FinishArea
+extends Area2D
+
+
+
+
+
+func _on_FinishArea2D_body_entered(_body):
+	pass # Replace with function body.
