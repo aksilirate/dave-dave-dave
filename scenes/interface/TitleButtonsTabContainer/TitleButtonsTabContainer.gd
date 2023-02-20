e1bee65844097fd3b0e2a2d5fac8a5a2f0cfcd14
axilirate <56825186+axilirate@users.noreply.github.com>
2023-02-20 23:27:58 +0200

    update

--- scenes/interface/TitleButtonsTabContainer/TitleButtonsTabContainer.gd
+++ scenes/interface/TitleButtonsTabContainer/TitleButtonsTabContainer.gd
@@ -7,13 +7,5 @@ func _on_PlayGenericButton_pressed():
 	current_tab = 1
 
 
-func _on_PlayBackGenericButton_pressed():
-	current_tab = 0
-
-
-func _on_AdvantureGenericButton_pressed():
-	current_tab = 2
-
-
 func _on_AdvantureBackGenericButton_pressed():
 	current_tab = 1
--- scenes/objects/Sign/Sign.gd
+++ scenes/objects/Sign/Sign.gd
@@ -3,13 +3,23 @@ extends Area2D
 
 onready var text_label = $TextLabel
 
+var tween: SceneTreeTween
 
 func _ready():
 	text_label.hide()
 
+
+
+
 func _on_Sign_body_entered(_body):
+	text_label.percent_visible = 0
+	tween = create_tween()
+	tween.tween_property(text_label, "percent_visible", 1.0, text_label.text.length() * 0.05)
 	text_label.show()
 
 
 func _on_Sign_body_exited(_body):
+	if is_instance_valid(tween):
+		tween.kill()
+	
 	text_label.hide()
